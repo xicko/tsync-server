ef413b90c643014e7d214d29354ef5dbe8648df9
xicko <gfieldmn@gmail.com>
2026-06-01 08:53:01 +0800

    (fix): import NotificationsSyncLog mongoose schema in notification sync module

--- src/notifications-sync/notifications-sync.module.ts
+++ src/notifications-sync/notifications-sync.module.ts
@@ -1,9 +1,16 @@
 import { Module, Global } from '@nestjs/common';
 import { NotificationsSyncService } from './notifications-sync.service';
 import { NotificationsSyncController } from './notifications-sync.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NotificationsSyncLogSchema } from 'src/schemas/notifications-sync-log.schema';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'NotificationsSyncLog', schema: NotificationsSyncLogSchema },
+    ]),
+  ],
   controllers: [NotificationsSyncController],
   providers: [NotificationsSyncService],
   exports: [NotificationsSyncService],
